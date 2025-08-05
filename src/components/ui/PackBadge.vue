66979d75208117fc416ffd544efd548e758143dd
ndambra <ndambra2@gmail.com>
2025-08-05 13:21:19 -0400

    create badges component

---
+++ src/components/ui/PackBadge.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-badge
+    rounded
+    :color="useBadgeColor(pack)"
+    :label="pack"
+    class="q-py-xs"
+  />
+</template>
+<script setup>
+/* imports */
+import { useBadgeColor } from 'src/use/useBadgeColor';
+
+defineProps(['pack']);
+
+</script>
